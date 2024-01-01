4008bad42f1f63dc08f4ecfb16d8c3f13d34567e
John Cronin <jncronin@tysos.org>
2024-01-01 14:36:48 +0000

    Add changes for basic sdl2 build

--- SDL2-2.28.5/include/SDL_config.h
+++ SDL2-2.28.5/include/SDL_config.h
@@ -49,6 +49,8 @@
 #include "SDL_config_emscripten.h"
 #elif defined(__NGAGE__)
 #include "SDL_config_ngage.h"
+#elif defined(__GAMEKID)
+#include "SDL_config_gamekid.h"
 #else
 /* This is a minimal configuration just to get SDL running on new platforms. */
 #include "SDL_config_minimal.h"
--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ make -C build/zlib install
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-m4.cmake -DCMAKE_INSTALL_PREFIX=~/src/gk -DCMAKE_FIND_ROOT_PATH=~/src/gk -S libpng-1.6.40 -B build/libpng
 make -C build/libpng install
 
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-m4.cmake -DCMAKE_INSTALL_PREFIX=~/src/gk -DCMAKE_FIND_ROOT_PATH=~/src/gk -DSDL_LIBC=ON -DSDL_PTHREADS=ON -S SDL2-2.28.5/ -B build/sdl2
+make -C build/sdl2 install

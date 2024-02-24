61de21449850d08bb1b2d2d17dfe3201fddca16b
John Cronin <jncronin@tysos.org>
2024-02-24 17:15:20 +0000

    lseek deferred return

--- gloss-gk/src-gloss/lseek.c
+++ gloss-gk/src-gloss/lseek.c
@@ -11,5 +11,19 @@ int _lseek(int file, int offset, int whence)
 	__syscall(__syscall_lseek, &ret, &p, &_errno);
 	if(ret == (off_t)-1)
 		errno = _errno;
+	if(ret == -2)	// Deferred return
+	{
+		struct WaitSimpleSignal_params wssp;
+		while(1)
+		{
+			uint32_t wss;
+			__syscall(WaitSimpleSignal, &wss, &wssp, NULL);
+			if(wss)
+				break;
+		}
+		if(wssp.ival1 == -1)
+			errno = wssp.ival2;
+		return wssp.ival1;
+	}
 	return ret;
 }

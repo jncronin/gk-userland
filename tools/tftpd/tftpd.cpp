995001f8c2b39ade7ab35a3b58a63a9881a1abf8
John Cronin <jncronin@tysos.org>
2024-03-17 12:41:30 +0000

    working tftpd

--- tools/tftpd/tftpd.cpp
+++ tools/tftpd/tftpd.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <cstring>
 #include <pthread.h>
+#include <unistd.h>
 
 struct thread_params
 {
@@ -18,6 +19,14 @@ struct thread_params
 };
 
 static void *tfer_thread(void *tp);
+
+static void send_error(int sockfd, const struct sockaddr_in *dest,
+    int err_number, std::string errmsg = "");
+static void send_ack(int sockfd, const struct sockaddr_in *dest,
+    int blockno);
+
+static void handle_write(int sockfd, const sockaddr_in *dest, FILE *f);
+static void handle_read(int sockfd, const sockaddr_in *dest, FILE *f);
 
 int main(int argc, char *argv[])
 {
@@ -54,7 +63,7 @@ int main(int argc, char *argv[])
 
         if(rf_ret < 2)
         {
-            printf("invalid packet length: %d\n", rf_ret);
+            printf("invalid packet length: %li\n", rf_ret);
             continue;
         }
 
@@ -98,7 +107,7 @@ int main(int argc, char *argv[])
                         m = thread_params::octet;
                     else
                     {
-                        printf("invalid mode: %s\n", mode);
+                        printf("invalid mode: %s\n", mode.c_str());
                         break;
                     }
 
@@ -133,6 +142,29 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+static void send_error(int sockfd, const struct sockaddr_in *dest,
+    int err_number, std::string errmsg)
+{
+    char buf[512];
+    *reinterpret_cast<uint16_t *>(&buf[0]) = htons(5);
+    *reinterpret_cast<uint16_t *>(&buf[2]) = htons(err_number);
+    strncpy(&buf[4], errmsg.c_str(), 511-4);
+    buf[511] = 0;
+
+    auto to_send = std::min(512U, (unsigned int)errmsg.length() + 5);
+
+    sendto(sockfd, buf, to_send, 0, (const sockaddr *)dest, sizeof(sockaddr_in));
+}
+
+static void send_ack(int sockfd, const struct sockaddr_in *dest,
+    int block_no)
+{
+    char buf[32];
+    *reinterpret_cast<uint16_t *>(&buf[0]) = htons(4);
+    *reinterpret_cast<uint16_t *>(&buf[2]) = htons(block_no);
+    sendto(sockfd, buf, 4, 0, (const sockaddr *)dest, sizeof(sockaddr_in));
+}
+
 static void *tfer_thread(void *_tp)
 {
     auto tp = reinterpret_cast<thread_params *>(_tp);
@@ -141,6 +173,7 @@ static void *tfer_thread(void *_tp)
     if(sockfd < 0)
     {
         printf("client socket failed %d\n", errno);
+        delete tp;
         return nullptr;
     }
 
@@ -160,6 +193,7 @@ static void *tfer_thread(void *_tp)
             {
                 // fail
                 printf("failed to open ephemereal port\n");
+                delete tp;
                 return nullptr;
             }
         }
@@ -169,7 +203,194 @@ static void *tfer_thread(void *_tp)
         }
     }
 
-    // now decide what to do based upon whether RRQ or WRQ
+    // open file, mode dependent upon RRQ vs WRQ
+    char fmode[8];
+    if(tp->is_write)
+    {
+        if(tp->mode == thread_params::netascii)
+        {
+            strcpy(fmode, "w");
+        }
+        else
+        {
+            strcpy(fmode, "wb");
+        }
+    }
+    else
+    {
+        if(tp->mode == thread_params::netascii)
+        {
+            strcpy(fmode, "r");
+        }
+        else
+        {
+            strcpy(fmode, "rb");
+        }
+    }
+    auto f = fopen(tp->fname.c_str(), fmode);
+    if(!f)
+    {
+        printf("error opening file %s: %d\n", tp->fname.c_str(), errno);
+        send_error(sockfd, &tp->peer_addr, 1);
+        close(sockfd);
+        delete tp;
+        return nullptr;
+    }
 
-    // TODO
+    if(tp->is_write)
+    {
+        handle_write(sockfd, &tp->peer_addr, f);
+    }
+    else
+    {
+        handle_read(sockfd, &tp->peer_addr, f);
+    }
+
+    close(sockfd);
+    fclose(f);
+    delete tp;
+    return nullptr;
+}
+
+static void handle_write(int sockfd, const sockaddr_in *dest, FILE *f)
+{
+    // writes start by acknowledging block 0 (i.e. the WRQ)
+    send_ack(sockfd, dest, 0);
+
+    int block_no = 1;
+    // then expect to receive packets and acknowledge them appropriately
+    while(true)
+    {
+        char buf[516];
+
+        sockaddr_in from = { 0 };
+        socklen_t fromlen = sizeof(sockaddr_in);
+        auto br = recvfrom(sockfd, buf, 516, 0, (sockaddr *)&from, &fromlen);
+
+        // ensure it is our peer
+        if(fromlen < sizeof(sockaddr_in) || memcmp(dest, &from, sizeof(sockaddr_in)) != 0)
+            continue;
+        
+        // check appropriate size
+        if(br < 4)
+        {
+            printf("packet size %d, %d\n", (int)br, errno);
+            return;
+        }
+
+        // check packet type
+        auto ptype = ntohs(*reinterpret_cast<uint16_t *>(&buf[0]));
+        if(ptype != 3)
+        {
+            printf("invalid ptype %d\n", ptype);
+            return;
+        }
+        
+        // ensure it is the next expected packet
+        auto cur_block_id = ntohs(*reinterpret_cast<uint16_t *>(&buf[2]));
+        if(cur_block_id != block_no)
+        {
+            printf("out of order packet %d\n", cur_block_id);
+            continue;
+        }
+
+        if(br > 4)
+        {
+            // try writing to file
+            auto bw = fwrite(&buf[4], 1, br - 4, f);
+            if(bw != br - 4)
+            {
+                printf("fwrite failed %d, %d\n", (unsigned int)bw, errno);
+                send_error(sockfd, dest, 0, "fwrite failed");
+                return;
+            }
+            printf("wrote %d bytes, pkt %d\n", (int)bw, cur_block_id);
+        }
+
+        // send ack
+        send_ack(sockfd, dest, block_no);
+        block_no++;
+
+        if(br < 516)
+        {
+            // done
+            return;
+        }
+    }
+}
+
+static void handle_read(int sockfd, const sockaddr_in *dest, FILE *f)
+{
+    int block_no = 1;
+
+    // reads start by writing data straight away
+    while(true)
+    {
+        char buf[516];
+
+        *reinterpret_cast<uint16_t *>(&buf[0]) = ntohs(3);
+        *reinterpret_cast<uint16_t *>(&buf[2]) = ntohs(block_no);
+
+        auto br = fread(&buf[4], 1, 512, f);
+        if(br < 0)
+        {
+            printf("fread failed %d\n", errno);
+            send_error(sockfd, dest, 0, "fread failed");
+            return;
+        }
+
+        auto bw = sendto(sockfd, buf, br + 4, 0, (const sockaddr *)dest, sizeof(sockaddr_in));
+        if(bw != br + 4)
+        {
+            printf("sendto failed %d, %d\n", (int)bw, errno);
+            send_error(sockfd, dest, 0, "sendto failed");
+            return;
+        }
+
+        // await ack
+        while(true)
+        {
+            sockaddr_in from = { 0 };
+            socklen_t fromlen = sizeof(sockaddr_in);
+            br = recvfrom(sockfd, buf, 516, 0, (sockaddr *)&from, &fromlen);
+
+            printf("recv %u\n", (unsigned int)br);
+
+            // ensure it is our peer
+            if(fromlen < sizeof(sockaddr_in) || memcmp(dest, &from, sizeof(sockaddr_in)) != 0)
+            {
+                printf("recv not from peer\n");
+                continue;
+            }
+            
+            // check appropriate size
+            if(br < 4)
+            {
+                printf("packet size %u, %d\n", (unsigned int)br, errno);
+                return;
+            }
+
+            // check packet type
+            auto ptype = ntohs(*reinterpret_cast<uint16_t *>(&buf[0]));
+            if(ptype != 4)
+            {
+                printf("invalid ptype %d\n", ptype);
+                return;
+            }
+
+            // check block_no
+            auto cur_block_id = ntohs(*reinterpret_cast<uint16_t *>(&buf[2]));
+            printf("ack for %u\n", cur_block_id);
+            if(cur_block_id == block_no)
+                break;
+        }
+
+        if(bw == 4)
+        {
+            // final packet
+            return;
+        }
+
+        block_no++;
+    }
 }

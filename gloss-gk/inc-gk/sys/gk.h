31ab76bae9b71e0e51f99f996432e5eaa0f21341
John Cronin <jncronin@tysos.org>
2024-03-31 18:18:58 +0100

    extra include file because newlib doesn't give us this

---
+++ gloss-gk/inc-gk/sys/gk.h
@@ -0,0 +1,22 @@
+#ifndef _SYS_GK_H
+#define _SYS_GK_H
+
+// extra GK defines not added by newlib nano
+#define FE_TONEAREST            0
+#define FE_TOWARDZERO           1
+#define FE_DOWNWARD             2
+#define FE_UPWARD               3
+
+#define __STRINGIFY(a) #a
+
+#define __INT64 "ll"
+#define __PRI64(x) __INT64 __STRINGIFY(x)
+
+#define PRId64		__PRI64(d)
+#define PRIi64		__PRI64(i)
+#define PRIo64		__PRI64(o)
+#define PRIu64		__PRI64(u)
+#define PRIx64		__PRI64(x)
+#define PRIX64		__PRI64(X)
+
+#endif
--- toolchain-m4.cmake
+++ toolchain-m4.cmake
@@ -17,7 +17,7 @@ set(CMAKE_C_STANDARD_LIBRARIES "-lm")
 set(CMAKE_CXX_STANDARD_LIBRARIES "-lm")
 
 set(COMMON_FLAGS "-mthumb -mcpu=${TARGET_CPU} -mfloat-abi=hard -mfpu=fpv5-sp-d16 -ffast-math")
-set(C_CXX_FLAGS  "--specs=nano.specs -ffunction-sections -fdata-sections -ffreestanding -D__GAMEKID__ -D_POSIX_THREADS -I$ENV{HOME}/src/gk/include")
+set(C_CXX_FLAGS  "-include sys/gk.h --specs=nano.specs -ffunction-sections -fdata-sections -ffreestanding -D__GAMEKID__ -D_POSIX_THREADS -I$ENV{HOME}/src/gk/include")
 set(CXX_FLAGS    "-fno-exceptions -fno-rtti -fno-threadsafe-statics -Wno-psabi")
 
 set(CMAKE_C_FLAGS_INIT          "${COMMON_FLAGS} ${C_CXX_FLAGS}"              CACHE STRING "" FORCE)

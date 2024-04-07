3ddf508742c99ed7fdc274c7be3d48102d7e5f7c
John Cronin <jncronin@tysos.org>
2024-04-07 12:39:38 +0100

    gpu raw interface

--- gloss-gk/inc-gk/gk.h
+++ gloss-gk/inc-gk/gk.h
@@ -12,6 +12,25 @@ extern "C"
 
 ssize_t GK_GPUEnqueueMessages(const struct gpu_message *msgs, size_t nmsg);
 
+struct __gk_cmd_list_header
+{
+    size_t __max_cmds;
+    size_t __ncmds;
+};
+
+#define GK_GPU_CommandList(name, ncmds) \
+    struct __gk_cmd_list_ ## name ## _ ## ncmds { \
+        struct __gk_cmd_list_header hdr; \
+        struct gpu_message msgs[ncmds]; \
+    } name = { { ncmds, 0 } };
+
+int GK_GPUResetCommandList(void *cmdlist);
+int GK_GPUClearScreen(void *cmdlist);
+int GK_GPUBlitScreen(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride);
+int GK_GPUBlitScreenNoBlend(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride);
+int GK_GPUFlipBuffers(void *cmdlist);
+int GK_GPUFlush(void *cmdlist);
+int GK_GPUCleanCache(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride);
 
 
 #ifdef __cplusplus
--- gloss-gk/src-gloss/gpu.cpp
+++ gloss-gk/src-gloss/gpu.cpp
@@ -3,6 +3,7 @@
 #include "_gk_gpu.h"
 #include <errno.h>
 #include "deferred.h"
+#include "gk.h"
 
 extern "C" ssize_t GK_GPUEnqueueMessages(const struct gpu_message *msgs, size_t nmsg)
 {
@@ -31,4 +32,187 @@ extern "C" ssize_t GK_GPUEnqueueMessages(const struct gpu_message *msgs, size_t 
     }
 
     return nsent;
+}
+
+int GK_GPUResetCommandList(void *cmdlist)
+{
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    hdr->__ncmds = 0;
+    return 0;
+}
+
+int GK_GPUClearScreen(void *cmdlist)
+{
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    if(hdr->__ncmds >= hdr->__max_cmds)
+        return -1;
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+    auto msg = &msgs[hdr->__ncmds++];
+
+    msg->type = ClearScreen;
+    msg->dest_addr = 0;
+    msg->dest_pf = 0;
+    msg->dx = 0;
+    msg->dy = 0;
+    msg->dw = 0;
+    msg->dh = 0;
+    msg->dp = 0;
+    msg->src_addr_color = 0;
+    msg->src_pf = 0;
+    return 0;
+}
+
+int GK_GPUBlitScreen(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride)
+{
+    uint32_t spf = 0;
+    switch(bpp)
+    {
+        case 16:
+            spf = GK_PIXELFORMAT_RGB565;
+            break;
+        case 32:
+            spf = GK_PIXELFORMAT_ARGB8888;
+            break;
+        default:
+            return -1;
+    }
+
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    if(hdr->__ncmds >= hdr->__max_cmds)
+        return -1;
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+    auto msg = &msgs[hdr->__ncmds++];
+
+    msg->type = BlitImage;
+    msg->dest_addr = 0;
+    msg->dest_pf = 0;
+    msg->dx = 0;
+    msg->dy = 0;
+    msg->dw = 0;
+    msg->dh = 0;
+    msg->dp = 0;
+    msg->src_addr_color = (uint32_t)(uintptr_t)src;
+    msg->sx = 0;
+    msg->sy = 0;
+    msg->w = w;
+    msg->h = h;
+    msg->sp = stride;
+    msg->src_pf = spf;
+
+    return 0;
+}
+
+int GK_GPUBlitScreenNoBlend(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride)
+{
+    uint32_t spf = 0;
+    switch(bpp)
+    {
+        case 16:
+            spf = GK_PIXELFORMAT_RGB565;
+            break;
+        case 32:
+            spf = GK_PIXELFORMAT_ARGB8888;
+            break;
+        default:
+            return -1;
+    }
+
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    if(hdr->__ncmds >= hdr->__max_cmds)
+        return -1;
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+    auto msg = &msgs[hdr->__ncmds++];
+
+    msg->type = BlitImageNoBlend;
+    msg->dest_addr = 0;
+    msg->dest_pf = 0;
+    msg->dx = 0;
+    msg->dy = 0;
+    msg->dw = 0;
+    msg->dh = 0;
+    msg->dp = 0;
+    msg->src_addr_color = (uint32_t)(uintptr_t)src;
+    msg->sx = 0;
+    msg->sy = 0;
+    msg->w = w;
+    msg->h = h;
+    msg->sp = stride;
+    msg->src_pf = spf;
+
+    return 0;
+}
+
+int GK_GPUFlush(void *cmdlist)
+{
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+
+    size_t ncmds = hdr->__ncmds;
+    if(ncmds > hdr->__max_cmds)
+        ncmds = hdr->__max_cmds;
+    auto ret = GK_GPUEnqueueMessages(msgs, ncmds);
+
+    if((ret >= 0) && ((size_t)ret == hdr->__ncmds))
+    {
+        // success, now wait for completion
+        gpu_message gsignal;
+        gsignal.type = SignalThread;
+        while(GK_GPUEnqueueMessages(&gsignal, 1) != 1);
+    }
+    if(ret > 0)
+        hdr->__ncmds -= ret;
+    return ret;
+}
+
+int GK_GPUFlipBuffers(void *cmdlist)
+{
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    if(hdr->__ncmds >= hdr->__max_cmds)
+        return -1;
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+    auto msg = &msgs[hdr->__ncmds++];
+
+    msg->type = FlipBuffers;
+
+    return 0;
+}
+
+int GK_GPUCleanCache(void *cmdlist, const void *src, size_t w, size_t h, size_t bpp, size_t stride)
+{
+    uint32_t spf = 0;
+    switch(bpp)
+    {
+        case 16:
+            spf = GK_PIXELFORMAT_RGB565;
+            break;
+        case 32:
+            spf = GK_PIXELFORMAT_ARGB8888;
+            break;
+        default:
+            return -1;
+    }
+
+    auto hdr = reinterpret_cast<__gk_cmd_list_header *>(cmdlist);
+    if(hdr->__ncmds >= hdr->__max_cmds)
+        return -1;
+    auto msgs = reinterpret_cast<gpu_message *>(&hdr[1]);
+    auto msg = &msgs[hdr->__ncmds++];
+
+    msg->type = CleanCache;
+    msg->dest_addr = (uint32_t)(uintptr_t)src;
+    msg->dest_pf = spf;
+    msg->dx = 0;
+    msg->dy = 0;
+    msg->dw = w;
+    msg->dh = h;
+    msg->dp = stride;
+    msg->src_addr_color = (uint32_t)(uintptr_t)src;
+    msg->sx = 0;
+    msg->sy = 0;
+    msg->w = w;
+    msg->h = h;
+    msg->sp = stride;
+    msg->src_pf = spf;
+
+    return 0;
 }

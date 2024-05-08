e6acd2f25dc4cba56ce355a1ae995d1e541e314f
John Cronin <jncronin@tysos.org>
2024-05-08 17:32:28 +0100

    add inithook userland side

---
+++ gloss-gk/src-gloss/inithook.cpp
@@ -0,0 +1,19 @@
+/* Newlib clobbers argc/argv during startup.  Here we restore them. */
+#include "syscalls.h"
+
+extern "C" void software_init_hook() __attribute__((naked));
+
+extern "C" void software_init_hook()
+{
+    __asm volatile
+    (
+        "mov r0, %0     \n"
+        "mov r1, lr     \n"
+        "sub sp, #8     \n"
+        "mov r2, sp     \n"
+        "svc #0         \n"
+        "pop {r0-r1}    \n"
+        "bx lr          \n"
+        :: "r"((int)__syscall_newlibinithook)
+    );
+}

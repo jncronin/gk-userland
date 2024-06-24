9228d9fa4eb9ccdb14d79261c0ef4055ea3da592
John Cronin <jncronin@tysos.org>
2024-06-24 16:40:15 +0100

    __aeabi_read_tp cannot clobber registers

--- gloss-gk/src-gloss/tls.cpp
+++ gloss-gk/src-gloss/tls.cpp
@@ -1,8 +1,21 @@
 #include "syscalls.h"
 
-extern "C" void *__aeabi_read_tp()
+extern "C" void *__aeabi_read_tp_c()
 {
     void *ptr;
     __syscall(__syscall_read_tp, (void *)&ptr, (void *)&ptr, nullptr);
     return ptr;
+}
+
+/* __aeabi_read_tp is not allow to clobber any registers except r0 */
+extern "C" void *__aeabi_read_tp() __attribute__((naked));
+
+extern "C" void *__aeabi_read_tp()
+{
+    __asm__ volatile (
+        "push { r1, r2, r3, lr }    \n"
+        "bl __aeabi_read_tp_c       \n"
+        "pop { r1, r2, r3, lr }     \n"
+        "bx lr                      \n"
+        ::: "memory");
 }

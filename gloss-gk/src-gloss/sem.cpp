5caf70dcf4fb599c336196beb80bf9917c733acb
John Cronin <jncronin@tysos.org>
2024-06-24 13:36:59 +0100

    userland sem* syscalls

--- gloss-gk/src-gloss/sem.cpp
+++ gloss-gk/src-gloss/sem.cpp
@@ -1,6 +1,12 @@
+#define _POSIX_READER_WRITER_LOCKS
+#define _UNIX98_THREAD_MUTEX_ATTRIBUTES
+
+
 #include <pthread.h>
 #include <semaphore.h>
 #include <errno.h>
+#include "syscalls.h"
+#include "deferred.h"
 
 extern "C" int sem_close(sem_t *sem)
 {
@@ -10,20 +16,34 @@ extern "C" int sem_close(sem_t *sem)
 
 extern "C" int sem_destroy(sem_t *sem)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return deferred_call(__syscall_sem_destroy, (void *)sem);
 }
 
 extern "C" int sem_getvalue(sem_t *sem, int *valout)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem || !valout)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    __syscall_sem_getvalue_params p { sem, valout };
+    return deferred_call(__syscall_sem_getvalue, &p);
 }
 
 extern "C" int sem_init(sem_t *sem, int pshared, unsigned int value)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    __syscall_sem_init_params p { sem, pshared, value };
+    return deferred_call(__syscall_sem_init, &p);
 }
 
 extern "C" sem_t *sem_open(const char *name , int oflag, ...)
@@ -34,20 +54,32 @@ extern "C" sem_t *sem_open(const char *name , int oflag, ...)
 
 extern "C" int sem_post(sem_t *sem)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return deferred_call(__syscall_sem_post, (void *)sem);
 }
 
 extern "C" int sem_timedwait(sem_t *sem, const timespec *abstime)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return deferred_call_with_retry(__syscall_sem_trywait, (void *)sem, abstime);
 }
 
 extern "C" int sem_trywait(sem_t *sem)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return deferred_call(__syscall_sem_trywait, (void *)sem);
 }
 
 extern "C" int sem_unlink(const char *name)
@@ -58,6 +90,10 @@ extern "C" int sem_unlink(const char *name)
 
 extern "C" int sem_wait(sem_t *sem)
 {
-    errno = EINVAL;
-    return -1;
+    if(!sem)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return deferred_call_with_retry(__syscall_sem_trywait, (void *)sem);
 }

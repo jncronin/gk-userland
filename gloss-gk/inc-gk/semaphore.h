ba0ff5f64983d1f9af029e4def512c59bb8d4097
John Cronin <jncronin@tysos.org>
2024-10-14 13:08:52 +0100

    add sem_clockwait

--- gloss-gk/inc-gk/semaphore.h
+++ gloss-gk/inc-gk/semaphore.h
@@ -18,6 +18,8 @@ sem_t *sem_open(const char *, int, ...);
 int    sem_post(sem_t *);
 int    sem_timedwait(sem_t *, const struct timespec *);
 int    sem_trywait(sem_t *);
+int    sem_clockwait(sem_t *sem, clockid_t clock_id,
+       const struct timespec *abstime);
 int    sem_unlink(const char *);
 int    sem_wait(sem_t *);
 

2e406fbebbac1a06f0aaf23c26aec18f29b5f7a9
John Cronin <jncronin@tysos.org>
2024-12-14 12:19:54 +0000

    cache lvgl inputs

--- lvgl-9.1.0/src/drivers/gk/lv_gk_input.c
+++ lvgl-9.1.0/src/drivers/gk/lv_gk_input.c
@@ -12,7 +12,22 @@ static void gk_mouse_read_cb(lv_indev_t *indev, lv_indev_data_t *data);
 static void gk_update_state();
 static enum _lv_key_t gk_key_to_lv(unsigned short gkkey);
 
-static lv_indev_data_t d_kbd, d_mouse, d_touch;
+/* Simple FIFO ring buffer, not thread safe */
+#define GK_RB_SIZE 8
+struct gk_rb
+{
+    lv_indev_data_t d[GK_RB_SIZE];
+    unsigned int rp, wp, init;
+};
+
+static int gk_rb_push(struct gk_rb *rb, const lv_indev_data_t *d);
+static int gk_rb_pop(struct gk_rb *rb, lv_indev_data_t *d);
+static unsigned int ptr_plus_one(unsigned int ptr);
+
+static struct gk_rb d_mouse, d_kbd;
+
+/* cache the previously returned value */
+static lv_indev_data_t cur_mouse, cur_kbd;
 
 void lv_gk_register_inputs()
 {
@@ -25,8 +40,14 @@ lv_indev_t *lv_gk_kbd_create()
     lv_indev_t *indev = lv_indev_create();
     if(indev == NULL) return NULL;
 
-    d_kbd.key = 0;
-    d_kbd.state = 0;
+    cur_kbd.btn_id = 0;
+    cur_kbd.key = 0;
+    cur_kbd.state = 0;
+    cur_kbd.continue_reading = 0;
+
+    d_kbd.rp = 0;
+    d_kbd.wp = 0;
+    d_kbd.init = 1;
 
     lv_indev_set_type(indev, LV_INDEV_TYPE_KEYPAD);
     lv_indev_set_read_cb(indev, gk_kbd_read_cb);
@@ -39,9 +60,14 @@ lv_indev_t *lv_gk_mouse_create()
     lv_indev_t *indev = lv_indev_create();
     if(indev == NULL) return NULL;
 
-    d_mouse.point.x = 0;
-    d_mouse.point.y = 0;
-    d_mouse.state = 0;
+    cur_mouse.point.x = 0;
+    cur_mouse.point.y = 0;
+    cur_mouse.state = 0;
+    cur_mouse.continue_reading = 0;
+
+    d_mouse.rp = 0;
+    d_mouse.wp = 0;
+    d_mouse.init = 1;
 
     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
     lv_indev_set_read_cb(indev, gk_mouse_read_cb);
@@ -53,17 +79,16 @@ void gk_kbd_read_cb(lv_indev_t *indev, lv_indev_data_t *data)
 {
     gk_update_state();
 
-    data->key = d_kbd.key;
-    data->state = d_kbd.state;
+    gk_rb_pop(&d_kbd, &cur_kbd);
+    memcpy(data, &cur_kbd, sizeof(lv_indev_data_t));
 }
 
 void gk_mouse_read_cb(lv_indev_t *indev, lv_indev_data_t *data)
 {
     gk_update_state();
 
-    data->point.x = d_mouse.point.x;
-    data->point.y = d_mouse.point.y;
-    data->state = d_mouse.state;
+    gk_rb_pop(&d_mouse, &cur_mouse);
+    memcpy(data, &cur_mouse, sizeof(lv_indev_data_t));
 }
 
 void gk_update_state()
@@ -71,38 +96,45 @@ void gk_update_state()
     struct Event ev;
     while(GK_EventPeek(&ev) > 0)
     {
+        lv_indev_data_t d;
+        memset(&d, 0, sizeof(lv_indev_data_t));
+
         switch(ev.type)
         {
             case KeyDown:
-                d_kbd.key = gk_key_to_lv(ev.key);
-                d_kbd.state = LV_INDEV_STATE_PRESSED;
+                d.key = gk_key_to_lv(ev.key);
+                d.state = LV_INDEV_STATE_PRESSED;
+                gk_rb_push(&d_kbd, &d);
                 break;
             case KeyUp:
-                d_kbd.key = gk_key_to_lv(ev.key);
-                d_kbd.state = LV_INDEV_STATE_RELEASED;
+                d.key = gk_key_to_lv(ev.key);
+                d.state = LV_INDEV_STATE_RELEASED;
+                gk_rb_push(&d_kbd, &d);
                 break;
             case MouseDown:
             case MouseUp:
             case MouseMove:
                 if(ev.mouse_data.buttons & 0x1)
                 {
-                    d_mouse.state = (ev.type == MouseUp) ? LV_INDEV_STATE_RELEASED : LV_INDEV_STATE_PRESSED;
+                    d.state = (ev.type == MouseUp) ? LV_INDEV_STATE_RELEASED : LV_INDEV_STATE_PRESSED;
                 }
 
                 if(ev.mouse_data.is_rel)
                 {
-                    d_mouse.point.x += ev.mouse_data.x;
-                    d_mouse.point.y += ev.mouse_data.y;
+                    d.point.x += ev.mouse_data.x;
+                    d.point.y += ev.mouse_data.y;
                 }
                 else
                 {
-                    d_mouse.point.x = ev.mouse_data.x;
-                    d_mouse.point.y = ev.mouse_data.y;
+                    d.point.x = ev.mouse_data.x;
+                    d.point.y = ev.mouse_data.y;
                 }
-                if(d_mouse.point.x < 0) d_mouse.point.x = 0;
-                if(d_mouse.point.x >= 640) d_mouse.point.x = 639;
-                if(d_mouse.point.y < 0) d_mouse.point.y = 0;
-                if(d_mouse.point.y >= 480) d_mouse.point.y = 479;
+                if(d.point.x < 0) d.point.x = 0;
+                if(d.point.x >= 640) d.point.x = 639;
+                if(d.point.y < 0) d.point.y = 0;
+                if(d.point.y >= 480) d.point.y = 479;
+
+                gk_rb_push(&d_mouse, &d);
 
                 break;
 
@@ -147,4 +179,51 @@ enum _lv_key_t gk_key_to_lv(unsigned short gkkey)
         default:
             return 0;
     }
+}
+
+static int gk_rb_push(struct gk_rb *rb, const lv_indev_data_t *d)
+{
+    if(!rb->init)
+        return -1;
+
+    unsigned int new_wp = ptr_plus_one(rb->wp);
+    if(new_wp == rb->rp)
+    {
+        return -1;
+    }
+    memcpy(&rb->d[rb->wp], d, sizeof(lv_indev_data_t));
+    rb->wp = new_wp;
+    return 1;
+}
+
+static int gk_rb_pop(struct gk_rb *rb, lv_indev_data_t *d)
+{
+    if(!rb->init)
+        return -1;
+    
+    if(rb->rp == rb->wp)
+    {
+        return 0;
+    }
+
+    memcpy(d, &rb->d[rb->rp], sizeof(lv_indev_data_t));
+    rb->rp = ptr_plus_one(rb->rp);
+    if(rb->rp == rb->wp)
+    {
+        d->continue_reading = 0;
+    }
+    else
+    {
+        d->continue_reading = 1;
+    }
+
+    return 1;
+}
+
+unsigned int ptr_plus_one(unsigned int ptr)
+{
+    ptr++;
+    if(ptr >= GK_RB_SIZE)
+        ptr = 0;
+    return ptr;
 }

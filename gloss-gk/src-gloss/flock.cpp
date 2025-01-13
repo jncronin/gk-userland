cde899209f8608236c944363ca63f23d5021ed0c
John Cronin <jncronin@tysos.org>
2025-01-13 14:37:23 +0000

    add flockfile/funlockfile

--- gloss-gk/src-gloss/flock.cpp
+++ gloss-gk/src-gloss/flock.cpp
@@ -1,8 +1,19 @@
 #include "syscalls.h"
 #include "deferred.h"
+#include <stdio.h>
 
 extern "C" int flock(int fd, int operation)
 {
     // not implemented
     return 0;
+}
+
+extern "C" void flockfile(FILE *f)
+{
+    // not implemented
+}
+
+extern "C" void funlockfile(FILE *f)
+{
+    // not implemented
 }

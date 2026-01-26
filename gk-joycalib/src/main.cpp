44cfc8350606ac8c38546c86d62c0e9e0eac70e0
John Cronin <jncronin@tysos.org>
2026-01-26 22:09:07 +0000

    show 3 axes

--- gk-joycalib/src/main.cpp
+++ gk-joycalib/src/main.cpp
@@ -26,7 +26,8 @@ int main()
     SDL_Event ev;
     bool quit = false;
 
-    std::list<pt> jpt;
+    std::list<pt> jpt[3];
+    auto nsticks = std::min(3U, (unsigned int)SDL_JoystickNumAxes(j));
 
     while(!quit)
     {
@@ -42,37 +43,44 @@ int main()
         if(quit)
             break;
 
-        auto x = SDL_JoystickGetAxis(j, 0);
-        auto y = SDL_JoystickGetAxis(j, 1);
-
-        printf("x: %d, y: %d\n", (int)x, (int)y);
-
-        jpt.push_front({ .x = x, .y = y });
-        while(jpt.size() >= 256)
+        for(auto stick = 0U; stick < nsticks; stick++)
         {
-            jpt.pop_back();
+            auto x = SDL_JoystickGetAxis(j, stick * 2 + 0);
+            auto y = SDL_JoystickGetAxis(j, stick * 2 + 1);
+
+            jpt[stick].push_front({ .x = x, .y = y });
+            while(jpt[stick].size() >= 256)
+            {
+                jpt[stick].pop_back();
+            }
         }
 
         SDL_SetRenderDrawColor(r, 0, 0, 0, 0);
         SDL_RenderClear(r);
 
-        auto intens = 255;
-        unsigned int last_x = ~0U;
-        unsigned int last_y = ~0U;
-        for(const auto &p : jpt)
+        for(auto stick = 0U; stick < nsticks; stick++)
         {
-            SDL_SetRenderDrawColor(r, intens, 0, 0, 255);
+            auto intens = 255;
+            unsigned int last_x = ~0U;
+            unsigned int last_y = ~0U;
+            for(const auto &p : jpt[stick])
+            {
+                SDL_SetRenderDrawColor(r, 
+                    (stick == 0) ? intens : 0,
+                    (stick == 1) ? intens : 0,
+                    (stick == 2) ? intens : 0, 255);
 
-            auto scr_x = (((int)p.x + 32768) * scr_w) / 65536;
-            auto scr_y = scr_h - (((int)p.y + 32768) * scr_h) / 65536;
+                auto scr_x = (((int)p.x + 32768) * scr_w) / 65536;
+                auto scr_y = (((int)p.y + 32768) * scr_h) / 65536;
 
-            if(last_x != ~0U && last_y != ~0U)
-                SDL_RenderDrawLine(r, last_x, last_y, scr_x, scr_y);
-            last_x = scr_x;
-            last_y = scr_y;
+                if(last_x != ~0U && last_y != ~0U)
+                    SDL_RenderDrawLine(r, last_x, last_y, scr_x, scr_y);
+                last_x = scr_x;
+                last_y = scr_y;
 
-            if(intens > 0)
-                intens--;
+                if(intens > 0)
+                    intens--;
+            }
         }
 
         SDL_RenderPresent(r);

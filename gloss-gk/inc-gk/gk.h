9ee301cd2b611663b29e36a3ec1b1c12decfb8e7
John Cronin <jncronin@tysos.org>
2026-01-29 21:58:40 +0000

    fix use of bool in C

--- gloss-gk/inc-gk/gk.h
+++ gloss-gk/inc-gk/gk.h
@@ -72,7 +72,7 @@ int GK_GetTiltAxes(int *x, int *y);
 #if __GAMEKID__ >= 4
 int GK_GetJoystickBAxes(int *x, int *y);
 
-int GK_GetJoystickAxesEx(unsigned int axis_pair, int *x, int *y, bool is_raw);
+int GK_GetJoystickAxesEx(unsigned int axis_pair, int *x, int *y, int is_raw);
 int GK_SetJoystickCalibration(unsigned int axis_pair,
     int left, int right, int top, int bottom, int middle_x, int middle_y);
 #endif

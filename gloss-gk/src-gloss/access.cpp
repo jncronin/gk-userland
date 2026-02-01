10282dc8474f2e8ed05f301982563a395679438b
John Cronin <jncronin@tysos.org>
2026-02-01 17:06:07 +0000

    implement access()

--- gloss-gk/src-gloss/access.cpp
+++ gloss-gk/src-gloss/access.cpp
@@ -1,6 +1,21 @@
 #include <unistd.h>
+#include <fcntl.h>
 
 extern "C" int access(const char *path, int mode)
 {
-    return -1;
+    int fmode = 0;
+    if(mode == W_OK)
+    {
+        fmode = O_RDWR;
+    }
+    else
+    {
+        fmode = O_RDONLY;
+    }
+    auto fd = open(path, fmode);
+    if(fd >= 0)
+    {
+        close(fd);
+    }
+    return (fd >= 0) ? 0 : -1;
 }

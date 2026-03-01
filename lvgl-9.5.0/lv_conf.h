0ed680fe7ce217b49c90274f3f64f033162647d8
John Cronin <jncronin@tysos.org>
2026-03-01 11:42:21 +0000

    allow code to overwrite lv_color_depth

--- lvgl-9.5.0/lv_conf.h
+++ lvgl-9.5.0/lv_conf.h
@@ -27,7 +27,9 @@
  *====================*/
 
 /** Color depth: 1 (I1), 8 (L8), 16 (RGB565), 24 (RGB888), 32 (XRGB8888) */
+#ifndef LV_COLOR_DEPTH
 #define LV_COLOR_DEPTH 16
+#endif
 
 /*=========================
    STDLIB WRAPPER SETTINGS

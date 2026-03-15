10e7e1838c917243b681e6e0b08ef824c83ce986
John Cronin <jncronin@tysos.org>
2026-03-15 08:38:55 +0000

    add thicker lines add digital deadzone + dividing lines

--- gk-joycalib/src/main.cpp
+++ gk-joycalib/src/main.cpp
@@ -8,6 +8,13 @@ const unsigned int scr_w = 640;
 const unsigned int scr_h = 480;
 
 static void do_calib(unsigned int axis_pair, const std::list<pt> &raw_pts);
+static void draw_thickline(SDL_Renderer * renderer, int x1, int y1, int x2, int y2, int thickness = 3);
+static int joy_to_screen(int v, unsigned int scr_size);
+static int joy_to_screen(double v, unsigned int scr_size);
+static int joyx_to_screen(int v);
+static int joyy_to_screen(int v);
+static int joyx_to_screen(double v);
+static int joyy_to_screen(double v);
 
 int main()
 {
@@ -81,6 +88,34 @@ int main()
         SDL_SetRenderDrawColor(r, 0, 0, 0, 0);
         SDL_RenderClear(r);
 
+        /* Draw digital bounding boxes */
+        // deadzone hard coded at 8000 - TODO: read from kernel somehow
+        const double deadzone = 8000.0;
+        const int circle_segments = 32;
+        SDL_Point cs[circle_segments + 1];
+        for(auto ccs = 0; ccs < circle_segments; ccs++)
+        {
+            auto ang = 2.0 * (double)M_PI / (double)circle_segments * (double)ccs;
+            auto x = (int)std::round((deadzone * std::cos(ang) + 32768.0) * (double)scr_w / 65536.0);
+            auto y = (int)std::round((deadzone * std::sin(ang) + 32768.0) * (double)scr_h / 65536.0);
+            cs[ccs].x = joyx_to_screen(deadzone * std::cos(ang));
+            cs[ccs].y = joyy_to_screen(deadzone * std::sin(ang));
+        }
+        cs[circle_segments].x = joyx_to_screen(deadzone);
+        cs[circle_segments].y = joyy_to_screen(0);
+        SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
+        SDL_RenderDrawLines(r, cs, circle_segments + 1);
+
+        for(auto ang_i = -7; ang_i <= 7; ang_i += 2)
+        {
+            auto ang = (double)ang_i * (double)M_PI / 8.0;
+            SDL_RenderDrawLine(r,
+                joyx_to_screen(deadzone * cos(ang)),
+                joyy_to_screen(deadzone * sin(ang)),
+                joyx_to_screen(32768.0 * cos(ang)),
+                joyy_to_screen(32768.0 * sin(ang)));
+        }
+
         for(auto stick = 0U; stick < nsticks; stick++)
         {
             auto intens = 255;
@@ -97,7 +132,7 @@ int main()
                 auto scr_y = (((int)p.y + 32768) * scr_h) / 65536;
 
                 if(last_x != ~0U && last_y != ~0U)
-                    SDL_RenderDrawLine(r, last_x, last_y, scr_x, scr_y);
+                    draw_thickline(r, last_x, last_y, scr_x, scr_y);
                 last_x = scr_x;
                 last_y = scr_y;
 
@@ -112,6 +147,17 @@ int main()
     }
 
     return 0;
+}
+
+static void draw_thickline(SDL_Renderer * renderer, int x1, int y1, int x2, int y2, int thickness)
+{
+    for(int y = -thickness/2; y <= -thickness/2 + thickness; y++)
+    {
+        for(int x = -thickness/2; x <= -thickness/2 + thickness; x++)
+        {
+            SDL_RenderDrawLine(renderer, x1 + x, y1 + y, x2 + x, y2 + y);
+        }
+    }
 }
 
 static void do_calib(unsigned int axis_pair, const std::list<pt> &raw_pts)
@@ -138,4 +184,34 @@ static void do_calib(unsigned int axis_pair, const std::list<pt> &raw_pts)
     }
 
     GK_SetJoystickCalibration(axis_pair, left, right, top, bottom, (int)centre.x, (int)centre.y);
+}
+
+static int joy_to_screen(int v, unsigned int scr)
+{
+    return ((v + 32768) * (int)scr) / 65536;
+}
+
+static int joy_to_screen(double v, unsigned int scr)
+{
+    return (int)std::round((v + 32768.0) * (double)scr / 65536.0);
+}
+
+static inline int joyx_to_screen(int v)
+{
+    return joy_to_screen(v, scr_w);
+}
+
+static inline int joyy_to_screen(int v)
+{
+    return joy_to_screen(v, scr_h);
+}
+
+static inline int joyx_to_screen(double v)
+{
+    return joy_to_screen(v, scr_w);
+}
+
+static inline int joyy_to_screen(double v)
+{
+    return joy_to_screen(v, scr_h);
 }

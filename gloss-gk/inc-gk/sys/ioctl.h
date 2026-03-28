2974dfd92cda47a2cd6a6388b170a7d3397bcc61
John Cronin <jncronin@tysos.org>
2026-03-28 16:24:13 +0000

    add ioctl list

--- gloss-gk/inc-gk/sys/ioctl.h
+++ gloss-gk/inc-gk/sys/ioctl.h
@@ -6,18 +6,9 @@ extern "C" {
 #endif
 
 int ioctl(int fd, unsigned long request, ...);
+
+#include "_gk_ioctls.h"
 	
-struct winsize
-{
-    unsigned short ws_row;	/* rows, in characters */
-    unsigned short ws_col;	/* columns, in characters */
-    unsigned short ws_xpixel;	/* horizontal size, pixels */
-    unsigned short ws_ypixel;	/* vertical size, pixels */
-};
-
-#define TIOCGWINSZ  1
-#define TIOCSCTTY   2
-
 #ifdef __cplusplus
 }
 #endif

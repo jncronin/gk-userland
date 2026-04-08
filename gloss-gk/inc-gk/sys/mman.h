a8ceb179fef215ce12e38f6da65eb624bd25a4e7
John Cronin <jncronin@tysos.org>
2026-04-08 12:01:30 +0100

    fix mmap flags so that they can be used as bitfields

--- gloss-gk/inc-gk/sys/mman.h
+++ gloss-gk/inc-gk/sys/mman.h
@@ -14,8 +14,8 @@ int munmap(void *addr, size_t length);
 
 #define PROT_READ       1
 #define PROT_WRITE      2
-#define PROT_EXEC       3
-#define PROT_NONE       4
+#define PROT_EXEC       4
+#define PROT_NONE       0
 
 #define MAP_SHARED      1
 #define MAP_PRIVATE     2

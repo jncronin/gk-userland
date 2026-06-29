2e18256f699775da304c30877308b891c0be7d22
John Cronin <jncronin@tysos.org>
2026-06-29 15:57:36 +0100

    add cfsetspeed declaration

--- gloss-gk/inc-gk/sys/termios.h
+++ gloss-gk/inc-gk/sys/termios.h
@@ -20,6 +20,7 @@ speed_t cfgetispeed(const struct termios *);
 speed_t cfgetospeed(const struct termios *);
 int     cfsetispeed(struct termios *, speed_t);
 int     cfsetospeed(struct termios *, speed_t);
+int     cfsetspeed(struct termios *, speed_t);
 int     tcdrain(int);
 int     tcflow(int, int);
 int     tcflush(int, int);

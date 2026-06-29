2d3b7524166faaa8e1e5504a9ba306e5fe33150d
John Cronin <jncronin@tysos.org>
2026-06-29 18:26:55 +0100

    add utimes implementation

--- gloss-gk/src-gloss/utime.cpp
+++ gloss-gk/src-gloss/utime.cpp
@@ -7,3 +7,9 @@ extern "C" int utime(const char *filename, const struct utimbuf *times)
     // not yet implemented
     return 0;
 }
+
+extern "C" int utimes(const char *filename, const struct timeval times[2])
+{
+    // not yet implemented
+    return 0;
+}

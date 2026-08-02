6a764858c91770583a09bcb49bed95546e1b6825
John Cronin <jncronin@tysos.org>
2026-08-02 16:50:50 +0100

    update sdl2 build to enable loadso

--- build-libraries-v4-static.sh
+++ build-libraries-v4-static.sh
@@ -53,7 +53,7 @@ cd ../..
 echo -n -e "create $SYSROOT/usr/lib/libGL2.a\naddlib $SYSROOT/usr/lib/libGL.a\naddlib $SYSROOT/usr/lib/libOSMesa.a\nsave\nend\n" | aarch64-none-gkos-ar -M
 mv $SYSROOT/usr/lib/libGL2.a $SYSROOT/usr/lib/libGL.a
 
-cmake $CMAKE_OPTS -DUNIX=ON -DSDL_LIBC=ON -DSDL_PTHREADS=ON -DSDL_THREADS=ON -DSDL_OPENGL=OFF -DSDL_OPENGLES=OFF -DSDL_CLOCK_GETTIME=ON -S SDL2-2.28.5/ -B build-v4/sdl2
+cmake $CMAKE_OPTS -DUNIX=ON -DSDL_LIBC=ON -DSDL_PTHREADS=ON -DSDL_THREADS=ON -DSDL_OPENGL=OFF -DSDL_OPENGLES=OFF -DSDL_CLOCK_GETTIME=ON -DSDL_LOADSO=ON -S SDL2-2.28.5/ -B build-v4/sdl2
 make -C build-v4/sdl2 -j16 install
 
 cmake $CMAKE_OPTS -DUNIX=ON jpeg-9f/ -B build-v4/jpeg
